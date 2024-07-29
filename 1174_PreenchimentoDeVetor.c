99237cbdf751a2cd0819d03249ccfff44c29b4ff
Eduardo N. Korte <dudu.korte@gmail.com>
2024-07-29 22:38:34 +0000

    1174

--- 1174_PreenchimentoDeVetor.c
+++ 1174_PreenchimentoDeVetor.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 int main(){
-
-    int c = 0;
-
+    
     for(int i = 0; i < 100; i++){
         double num;
         scanf("%lf", &num);
-        num <= 10 ? printf("A[%d] = %.1lf\n", c, num) : 0;
-        num <= 10 ? c++ : 0;
+        num <= 10 ? printf("A[%d] = %.1lf\n", i, num) : 0;
     }
-
+    
     return 0;
 }
