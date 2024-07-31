e73e97a4bc77782833e198c2c0d85d1aa8b4b324
Eduardo N. Korte <dudu.korte@gmail.com>
2024-07-31 16:51:38 +0000

    1183-1190(Matrizes hihihih)

---
+++ 1190_AreaDireita.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    //declara matriz
+    double matrix[12][12];
+    double resultado = 0, elements = 0;
+    char op;
+    scanf(" %c", &op);
+
+    //Ler valores para a matriz
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            scanf("%lf", &matrix[i][j]);
+        }
+    }
+
+    //Calcular operação na matriz
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            if((i > 11-j) && (j > i)){
+                resultado += matrix[i][j];
+                elements++;
+            }
+        }
+    }
+
+    printf("%.1lf\n", op == 'S' ? resultado : resultado/elements);
+
+    return 0;
+}
