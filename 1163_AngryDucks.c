1b3771a7d6da9b064e5f4eb56481681d7063f0c5
Eduardo N. Korte <dudu.korte@gmail.com>
2024-08-09 00:08:59 +0000

    ANGRY DUCKS DFTC

--- 1163_AngryDucks.c
+++ 1163_AngryDucks.c
@@ -2,20 +2,30 @@
 #include <math.h>
 
 int main(){
-
-    const double g = 9.80665;
-    const double pi = 3.14159;
-    double h, vo, a;
+    long double h, vo, a, vox, voy, alcance, h0, d, pi = 3.14159, g = 9.80665 ;
     int nlogoina_inicio, nlogonia_final, n;
-    scanf("%lf", &h);
-    scanf("%d %d", &nlogoina_inicio, &nlogonia_final);
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++){
-        scanf("%lf %lf", &a, &vo);
-        a = (a * (pi / 180)) * 2; // convert from degrees to radians
 
-        double d = (pow(vo, 2) * sin(a))/g + h;
-        printf("%.5lf\n", d);
+    while(scanf("%Lf", &h0) != EOF){
+        h = 0.0;
+        scanf("%d %d", &nlogoina_inicio, &nlogonia_final);
+        scanf("%d", &n);
+
+        for(int i = 0; i < n; i++){
+            scanf("%Lf %Lf", &a, &vo);
+            
+            a = ((a * pi) / 180.0); // convert from degrees to radians
+
+            voy = vo*sin(a);
+            vox = vo*cos(a);
+
+            d = ((vo * sin(a)) * (vo * sin(a))) / (2 * g);
+            h = h0 + d;
+            
+            d = ((vox*voy)/g) +  (vox * (sqrt((2*h)/g)));
+
+            printf("%.5Lf -> %s\n", d, d > nlogoina_inicio && d < nlogonia_final ? "DUCK" : "NUCK");
+            
+        }
     }
 
     return 0;
