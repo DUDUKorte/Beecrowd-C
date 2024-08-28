af1953271e09c5dc0d889c90ce8c509c8e8402f5
Eduardo N. Korte <dudu.korte@gmail.com>
2024-08-28 23:35:27 +0000

    Trabalho Pronto

---
+++ C_Geral/trabalho_1_bim/EscadaRolante.c
@@ -0,0 +1,21 @@
+// Feito - Problewma 6
+
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    scanf("%d", &n);
+    int tempo_anterior = -1, segundos = 10;
+    for(int i = 0; i < n; i++){
+        int tempo_atual;
+        scanf("%d", &tempo_atual);
+        tempo_anterior == -1? tempo_anterior = tempo_atual : 0; // Adicionar a primeira passagem
+        segundos += tempo_atual - tempo_anterior;
+        tempo_anterior = tempo_atual;
+    }
+
+    printf("%d\n", segundos);
+
+    return 0;
+}
