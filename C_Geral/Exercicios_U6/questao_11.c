e521191412837cb3796e45898aaa86f90550679a
Eduardo N. Korte <dudu.korte@gmail.com>
2024-09-09 15:04:17 -0300

    Update questao_11.c

--- C_Geral/Exercicios_U6/questao_11.c
+++ C_Geral/Exercicios_U6/questao_11.c
@@ -1,16 +1,65 @@
 #include<stdio.h>
 
 typedef struct{
-    int coeficientes[50];
+    double coeficientes[50];
     int grau;
 } polinomio;
 
 void readPolinomio(polinomio *P){
     scanf("%d", &P->grau);
 
-    for(int i = -1; i < P->grau; i++){
-        scanf("%d", &P->coeficientes[i]);
+    for(int i = P->grau; i >= 0; i--){
+        scanf("%lf", &P->coeficientes[i]);
     }
+}
+
+void PrintPolinomio(polinomio P){
+    for(int i = P.grau; i >= 0; i--){
+        printf("%+.2fX^%d%s", P.coeficientes[i], i, i-1 < 0 ? "\n" : " ");
+    }
+}
+
+polinomio AddPolinomio(polinomio _A, polinomio _B){
+    polinomio P;
+
+    if(_A.grau > _B.grau){
+        P.grau = _A.grau;
+    }else{
+        P.grau = _B.grau;
+    }
+
+    for(int i = 0; i <= P.grau; i++){
+        P.coeficientes[i] = 0;
+    }
+
+    for(int i = 0; i <= P.grau; i++){
+        P.coeficientes[i] = _A.coeficientes[i] + _B.coeficientes[i];
+    }
+
+    return P;
+}
+
+polinomio multPolinomio(polinomio _A, polinomio _B){
+    polinomio P;
+    P.grau = _A.grau + _B.grau;
+
+    for(int i = 0; i <= P.grau; i++){
+        P.coeficientes[i] = 0;
+    }
+
+    for(int i = 0; i <= _A.grau; i++){
+        for(int j = 0; j <= _B.grau; j++){
+            P.coeficientes[i+j] += _A.coeficientes[i]*_B.coeficientes[j];
+        }
+    }
+
+    return P;
+}
+
+polinomio divPolinomio(polinomio _A, polinomio _B){
+    polinomio P;
+    
+    return P;
 }
 
 int main(){
@@ -20,6 +69,16 @@ int main(){
     readPolinomio(&A);
     scanf(" %c ", &oper);
     readPolinomio(&B);
+
+    PrintPolinomio(A);
+    PrintPolinomio(B);
+
+    polinomio aux;
+    aux = AddPolinomio(A, B);
+    PrintPolinomio(aux);
+    
+    aux = multPolinomio(A, B);
+    PrintPolinomio(aux);
 
     return 0;
 }
