35778b03568ba275107474517ad66dfd3c2ac8f0
Eduardo N. Korte <dudu.korte@gmail.com>
2024-09-23 17:53:37 +0000

    Vector dinamico main.c

--- C_Geral/vetores_dinamicos/main.c
+++ C_Geral/vetores_dinamicos/main.c
@@ -4,8 +4,22 @@
 #include <stdlib.h>
 
 int main(){
+    // Declaração de variáveis
+    Vector V1;
+    unsigned n;
 
+    // inicialização do Vector
+    printf("Exemplo de Vetor dinâmico\n");
+    printf("No. de Elementos do Vector? >> ");
+    scanf("%u", &n);
+    if(initVector(&V1, n)){
+        printf("ERRO: Memória insuficiente\n");
+        exit(1);
+    }
+    // Imprime o vetor inicial
+    printf("Vector Inicial:\n");
+    printVector(V1);
+    printf("\n");
     
-
     return 0;
 }
